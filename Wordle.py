0c295a11506c0c9289861ae88e5c6826bfdd462d
bbundy12 <brendanbundy1@gmail.com>
2024-01-10 14:07:35 -0700

    Milestone 1

--- Wordle.py
+++ Wordle.py
@@ -16,9 +16,19 @@ def wordle():
         gw.show_message("You have to implement this method.")
 
     gw = WordleGWindow()
+
+    # Pick a random word from the list of five-letter words
+    chosen_word = random.choice(FIVE_LETTER_WORDS)
+
+    # Display the chosen word in the first row of boxes
+    for i in range(N_COLS):
+        letter = chosen_word[i]
+        gw.set_square_letter(0, i, letter)
+
     gw.add_enter_listener(enter_action)
 
 # Startup code
 
 if __name__ == "__main__":
     wordle()
+    
